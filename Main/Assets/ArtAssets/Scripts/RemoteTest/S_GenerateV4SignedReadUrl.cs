3e6f7102e5b50b7bc4df75e1ed2f6976b18e66a4
彭暐翰(Leier) <weihan.peng@365palm.com.tw>
2023-02-24 18:38:51 +0800

    【試做】遠端 Bundle。

---
+++ Main/Assets/ArtAssets/Scripts/RemoteTest/S_GenerateV4SignedReadUrl.cs
@@ -0,0 +1,29 @@
+﻿using Google.Cloud.Storage.V1;
+using System;
+using System.IO;
+using System.Net.Http;
+using UnityEngine;
+
+public class S_GenerateV4SignedReadUrl : MonoBehaviour
+{
+    public string BucketName;
+
+    readonly TextAsset _key;
+
+    public string Func_GetSignedURL(string objectPathOnCloud)
+    {
+        var signURL = GenerateV4SignedReadUrl(BucketName, objectPathOnCloud);
+        return signURL;
+    }
+
+    public string GenerateV4SignedReadUrl(string bucketName = "your-unique-bucket-name", string objectName = "your-object-name")
+    {
+        var path = $"{Application.persistentDataPath}/Credentical.json";
+        File.WriteAllText(path, $"{_key}");
+
+        var urlSigner = UrlSigner.FromServiceAccountPath(path);
+        var url = urlSigner.Sign(bucketName, objectName, TimeSpan.FromSeconds(50), HttpMethod.Get);
+        File.WriteAllText(path, "");
+        return url;
+    }
+}
---
+++ Main/Assets/ArtAssets/Scripts/RemoteTest/S_SignURL.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.ResourceLocations;
+
+public class S_SignURL : MonoBehaviour
+{
+    //public S_GenerateV4SignedReadUrl SignURL;
+
+    //void Start()
+    //{
+    //    Addressables.ResourceManager.InternalIdTransformFunc += TranformFuncContent;
+    //}
+
+    //string TranformFuncContent(IResourceLocation location)
+    //{
+    //    // TestCloud....bundle (begin with Remote Load path on Addressable Asset Setitng)
+    //    string oldContentURL = null;
+    //    // https//..
+    //    string currentURLToUse = null;
+
+    //    if (location.InternalId.StartsWith("TestCloud"))
+    //    {
+    //        oldContentURL = location.InternalId;
+    //        Debug.Log($"Use Signed URL: {location.InternalId}");
+    //    }
+    //}
+}
